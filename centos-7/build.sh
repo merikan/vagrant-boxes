ac43b45f4b9bc868f04967a9143f71bec92f9bae
Peter Merikan <peter@merikan.com>
2015-11-06 21:31:51 +0100

    Every build should have its own template file

--- centos-7/build.sh
+++ centos-7/build.sh
@@ -2,11 +2,25 @@
 
 set -e
 
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    echo "Use one of the following Packer template files as argument"
+    for f in *.json; do echo "./$f"; done
+    exit 1
+fi
+
+if [ ! -f $1 ]; then
+    echo "Template file not found!"
+    exit 1
+fi
+
+NAME=$(basename $1 .json)
 : ${BUILD_VERSION:="v$(date +'%Y%m%d')"}
 export BUILD_VERSION
 
-rm -i builds/*-${BUILD_VERSION}.box || true
-packer build template.json
-shasum -a 256 builds/*-${BUILD_VERSION}.box
+rm -i builds/$NAME-${BUILD_VERSION}.box || true
+packer build $1
+shasum -a 256 builds/$NAME-${BUILD_VERSION}.box
 
 # $ vagrant box add builds/centos-7-x86_64-desktop-vbox-puppet-v20151026.box  --name centos-7-x86_64-desktop-vbox-puppet --force
